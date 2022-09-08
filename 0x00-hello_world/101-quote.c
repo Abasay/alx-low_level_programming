c4c845748db90d0e5d853acabd233b773bbc41c3
Abasay <abdulsalamasheem@gmail.com>
2022-09-08 07:51:04 -0700

    new

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the program starts and eds with main
+ * Return: returns 1 when error
+ */
+int main(void)
+{
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
