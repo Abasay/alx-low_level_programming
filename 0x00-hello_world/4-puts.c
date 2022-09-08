4dd4599262c8cc7e8fcb845860e67bab765c5bfd
Abasay <abdulsalamasheem@gmail.com>
2022-09-08 06:29:12 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-/**
- *main function- main body of the program
- */
-int main(void)
+int main(void)/* the main function */
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+ return (0);/* the functipn returns zero */
 }
