a5ef43ae2402a7ad23b321a6fae31ebf68dc4a96
Abasay <abdulsalamasheem@gmail.com>
2022-09-08 06:54:42 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main(void) /** execution starts and ends with the main function */
+/**
+ *main - the program starts and ends with the main
+ *Return : the main function returns zero when success
+ */
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return (0);/* the functipn returns zero */
+return (0);
 }
