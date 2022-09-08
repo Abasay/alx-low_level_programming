1f37d2bfeb93839c5ffad0b1edc6b8c891a44664
Abasay <abdulsalamasheem@gmail.com>
2022-09-08 04:43:40 -0700

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-int main(void) {
+/**
+ *main function- this is the function main which encompasses allvthe activity of the c program
+ */
+int main(void)
+{
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
-    }
+}
