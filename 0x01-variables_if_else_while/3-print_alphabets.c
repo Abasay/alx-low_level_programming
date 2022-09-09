3093c9fccbbc60820cdab147e24d4fd6b2422bef
Abasay <abdulsalamasheem@gmail.com>
2022-09-09 10:30:07 -0700

    alphabet except q and e

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - body of function
+ * Return: returns 0 when successful
+ */
+int main(void)
+{
+        char i;
+
+        i = 'a';
+        while (i <= 'z')
+        {
+                putchar(i);
+                i++;
+        }
+
+	i = 'A';
+	while (i <= 'Z')
+	{
+		putchar(i);
+		i++;
+	}
+        putchar('\n');
+        return (0);
+}
