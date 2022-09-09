0ce87f6a5a6492442f191bb9df1ca052587e2ca0
Abasay <abdulsalamasheem@gmail.com>
2022-09-09 16:08:05 -0700

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: retutn 0 for success
+ */
+int main(void)
+{
+	int i, j;
+
+	i = j = 48;
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			if ((i != j) && (i < j))
+			{
+				putchar(i);
+				putchar(j);
+				if ( i == 57 && i == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
