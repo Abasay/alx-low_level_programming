ab22a2f515544953c826a1d9b9cdde4a8ff16224
Abasay <abdulsalamasheem@gmail.com>
2022-09-13 10:15:13 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check code
+ *
+ * Return: return per instruction
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) && (c >= 97 && c <= 122)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+}
