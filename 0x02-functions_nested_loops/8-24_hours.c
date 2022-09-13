dfcc970bf4caa365f7d79d06ac801d53529e4264
Abasay <abdulsalamasheem@gmail.com>
2022-09-13 13:55:34 -0700

    24hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * jack_bauer - print hours
+ * Return: return 0;
+ */
+void jack_bauer(void)
+{
+	for (i = 48; i <= 51; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 53; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					if (i >= 50)
+					{
+						if (j >= 52)
+						{
+							break;
+						}
+					}
+					if (i > 50)
+					{
+						break;
+					}
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
