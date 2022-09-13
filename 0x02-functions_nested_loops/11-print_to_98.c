3c12843ed1cc9feb4acc55e0a5653304212dcc59
Abasay <abdulsalamasheem@gmail.com>
2022-09-13 15:15:35 -0700

    edit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,12 @@ void print_to_98(int n)
 
 	for (i = n; i < 98; i++)
 	{
-		printf("%i", i);
+		printf("%i, ", i);
 	}
 
 	for (i = n; i > 98; i--)
 	{
-		printf("%i", i);
+		printf("%i, ", i);
 	}
 	printf("98");
 	putchar('\n');
