87967ce1aee5aaac7e87b860ebe46e2f2ca35c58
Abasay <abdulsalamasheem@gmail.com>
2022-09-13 12:02:49 -0700

    new

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @n : parameter
+ * Return: return 0;
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else if (n == 0)
+	{
+		return (n * 0);
+	}
+	else if (n > 0)
+	{
+		return (n * 1);
+	}
+}
