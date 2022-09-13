ca9baf8b78ff0b0a7552b2ff096f07896dc5ceb0
Abasay <abdulsalamasheem@gmail.com>
2022-09-13 03:09:53 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - the function starts and end with main
+ * Return: return 0 when success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('\n');
+	return (0);
+}
