804a47f06c66f12dd172d7aa71296c0550e11b7f
Abasay <abdulsalamasheem@gmail.com>
2022-09-14 03:07:55 -0700

    new

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,26 +15,23 @@ void print_times_table(int n)
 	}
 	while (a <= n)
 	{
-		for (b =0; b <= n; b++)
+		for (b = 0; b <= n; b++)
 		{
 			rep = a * b;
 			if (b == 0)
 			{
 				_putchar('0' + rep);
-			}
-			else if (rep < 10)
+			}else if (rep < 10)
 			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar('0' + rep);
-			}
-			else if (rep < 100)
+			}else if (rep < 100)
 			{
 				_putchar(' ');
-				_putchar('0' + rep /10);
+				_putchar('0' + rep / 10);
 				_putchar('0' + rep % 10);
-			}
-			else
+			}else
 			{
 				_putchar('0' + rep / 100);
 				_putchar('0' + (rep - 100) / 10);
