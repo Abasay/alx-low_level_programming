e9482e79cf9c300cdbec847d9a9ff83029dbb8e5
Abasay <abdulsalamasheem@gmail.com>
2022-09-15 03:17:17 -0700

    new

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *  more_numbers - print more numbers
+ * Return: return void
+ */
+
+void more_numbers(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 48; j <= 58; j++)
