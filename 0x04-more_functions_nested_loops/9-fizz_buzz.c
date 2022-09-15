85fef837fcd5f3ae7d83d020661c8e80dce55ef1
Abasay <abdulsalamasheem@gmail.com>
2022-09-15 12:46:08 -0700

    new

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			if ((i % 5) == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+				printf("Fizz ");
+			}
+		}
+		else if ((i % 5) == 0)
+		{
+			if ((i % 3) == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+
+	return (0);
+}
