a804c1a71066a7b2f6954be8e6df87dddcc4a1c7
Abasay <abdulsalamasheem@gmail.com>
2022-09-19 07:35:20 -0700

    new

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s : parameter s
+ * Return: returns void
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (j = i; j >= 0; j--)
+	{
+		s[j];
+	}
+}
