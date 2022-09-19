9dc64870b5ac839a4a0898fcc4ad97cf3e2b6145
Abasay <abdulsalamasheem@gmail.com>
2022-09-19 09:20:44 -0700

    new

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - printing half
+ * @str: parameter string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i, j, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	if (i % 2 == 0)
+	{
+		for (j = (i / 2); j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+	else if (i % 2 != 0)
+	{
+		n = (i - 1) / 2;
+		for (j = n; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+}
