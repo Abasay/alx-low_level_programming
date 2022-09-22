0991aa882740b1b4735007b2c99fafe49e90855f
Abasay <abdulsalamasheem@gmail.com>
2022-09-22 03:33:39 -0700

    new

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,20 +13,26 @@ char *cap_string(char *str)
 
 	for (j = 0; str[j] != '\0'; j++)
 	{
-		if (str[0] >= 97 || str[0] <= 122)
+		if (str[0] >= 97 && str[0] <= 122)
 			str[0] = str[0] - 32;
 		for (k = 0; k < 3; k++)
 			if (str[j] == seps[k])
 			{
 				j = j + 1;
-				str[j] = str[j] - 32;
+				if (str[j] >= 97 && str[j] <= 122)
+				{
+					str[j] = str[j] - 32;
+				}
 				j = j - 1;
 			}
 		for (l = 3; l < 13; l++)
 			if (str[j] == *(seps + k))
 			{
 				j = j + 2;
-				str[j] = str[j] - 32;
+				if (str[j] >= 97 && str[j] <= 122)
+				{
+					str[j] = str[j] - 32;
+				}
 				j = j - 2;
 				break;
 			}
