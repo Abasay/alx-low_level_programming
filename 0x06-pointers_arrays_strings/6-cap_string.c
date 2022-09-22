41bdc80f958d8350d0ca6e8de9427cd983b89c27
Abasay <abdulsalamasheem@gmail.com>
2022-09-22 03:19:09 -0700

    new

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,10 +9,9 @@
 char *cap_string(char *str)
 {
 	int j, k, l;
-	char temp;
 	int seps[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	for (j = 0; str[i] != '\0'; j++)
+	for (j = 0; str[j] != '\0'; j++)
 	{
 		if (str[0] >= 97 || str[0] <= 122)
 			str[0] = str[0] - 32;
