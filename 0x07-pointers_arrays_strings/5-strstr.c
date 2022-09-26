293376f2195cfe335933f889af226d3d03ecee62
Abasay <abdulsalamasheem@gmail.com>
2022-09-26 07:57:17 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - string check
+ * @haystack: destination
+ * @needle: what to find
+ * Return: return string
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *str;
+	
+	while (*haystack != '\0')
+	{
+		str = needle;
+		while (*haystack != '\0' && *str != '\0' && *haystack == *str)
+		{
+			haystack++;
+			str++;
+		}
+		if (str == '\0')
+			return (haystack);
+		haystack = haystack + 1;
+	}
+	return (0);
+}
