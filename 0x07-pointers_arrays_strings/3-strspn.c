19674ac9190a8c2247c1892a473dfdc737e7ff49
Abasay <abdulsalamasheem@gmail.com>
2022-09-26 04:49:31 -0700

    new

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - string prefix check
+ * @s : destination to check
+ * @accept: accept string
+ * Return: return integer
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k;
+
+	k = 0;
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+			if (accept[i] == s[j])
+			{
+				k++;
+				break;
+			}
+	}
+
+	return (k);
+}
