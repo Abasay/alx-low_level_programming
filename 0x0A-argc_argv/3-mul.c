1c8209dcbbdb5ec1e67675274c0eaa4e22924cea
Abasay <abdulsalamasheem@gmail.com>
2022-09-30 08:43:56 -0700

    atoi(argv[2]

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,18 @@
 
 int main(int argc, char *argv[])
 {
-	int result;
+	int result, num1, num2;
 
-	if (argc < 2)
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (argc < 2 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	result = atoi(argv[1]) * atoi(argv[2]);
+	result = num1 * num2;
 	printf("%d\n", result);
 
 	return (0);
