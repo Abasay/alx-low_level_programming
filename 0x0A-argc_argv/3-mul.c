7086f2ba6f5f3b77f54f065c518992be30953d22
Abasay <abdulsalamasheem@gmail.com>
2022-09-30 09:26:25 -0700

    new

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,14 +13,14 @@ int main(int argc, char *argv[])
 {
 	int result, num1, num2;
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	if (argc < 2 || argc > 2)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	num1 = atoi(argv[1]);
+        num2 = atoi(argv[2]);
 
 	result = num1 * num2;
 	printf("%d\n", result);
