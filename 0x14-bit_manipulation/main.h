0642d08bacc3f6eb5f40244768a663a1d026a0e3
Abasay <abdulsalamasheem@gmail.com>
2022-10-27 09:11:26 -0700

    0-binary_to_uint.c and main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/**
+ * File: MAIN_H - header gile containing the prototypes os all fumctions
+ *
+ * Desc: Header file for 0x14-bits_manipulation project
+ */
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif /* #ifndef MAIN_H */
