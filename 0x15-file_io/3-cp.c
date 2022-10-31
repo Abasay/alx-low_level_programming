5457eaae47a6c3c64df4c3f30b1e12efc3742320
Abasay <abdulsalamasheem@gmail.com>
2022-10-31 15:00:29 -0700

    working with files in and out

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,12 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * error_file - checks if file can be opened
- * @file_from: file from
- * @file_to: file to
- * @argv: arguments vector
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -14,21 +15,19 @@ void error_file(int file_from, int file_to, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(98);
+		exit(99);
 	}
 }
 
 /**
- * main - main func
- * @argc: arg count
- * @argv: arg vector
- * Return: 0 for success
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
@@ -40,12 +39,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
+
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
 	nchars = 1024;
-
 	while (nchars == 1024)
 	{
 		nchars = read(file_from, buf, 1024);
@@ -71,3 +70,4 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
+
